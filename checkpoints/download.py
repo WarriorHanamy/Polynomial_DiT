f31c8f04e5fa045bc08c7bbaaadfe60bb54816f1
Weiliang Deng <Dengwl.2021@gmail.com>
2026-04-25 16:36:15 +0800

    fix: update download

--- checkpoints/download.py
+++ checkpoints/download.py
@@ -1,17 +1,12 @@
-
 from huggingface_hub import snapshot_download
 
-# 设置 Token 和仓库 ID
-TOKEN = "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"
 REPO_ID = "dwl1437/Polynomial-DiT"
 
-# 下载权重到本地目录
 snapshot_download(
     repo_id=REPO_ID,
     repo_type="model",
     local_dir="Polynomial-DiT-local",
-    token=TOKEN,
     resume_download=True,
     local_dir_use_symlinks=False,
-    ignore_patterns=[".git/*"]
+    ignore_patterns=[".git/*"],
 )
